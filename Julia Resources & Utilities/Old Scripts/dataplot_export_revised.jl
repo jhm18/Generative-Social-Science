024bc4c3ffa95a077e0087fc09cb4388ec0eebac
Jonathan H. Morgan, Ph.D <Jonathan.H.Morgan@gmail.com>
2022-04-06 14:21:08 -0400

    Revised the Dimension Function to be More Robust
    Dataplot assumes that there is a minimum of 1000 rows if the DIMENSION command is used.
    Consequently, for datasets that exceed 10 columns but are under 1000 rows, a 1000 rows must be specified rather than relying on the row count of the dataset.

--- Julia Resources & Utilities/Old Scripts/dataplot_export_revised.jl
+++ Julia Resources & Utilities/Old Scripts/dataplot_export_revised.jl
@@ -145,7 +145,12 @@
             #   Specifying Dataplot IO Settings
                 io_commands = vec(fill("a", 4, 1))
                 if (size(base_data)[2] > 10)
-                    io_commands[1] = string("DIMENSION", " ", size(base_data)[1], " ", "ROWS")
+                    if (size(data_frame)[1] < 1000 )
+                        io_commands[1] = string("DIMENSION", " ","1000", " ", "ROWS")
+                    else
+                        io_commands[1] = string("DIMENSION", " ", size(base_data)[1], " ", "ROWS")
+                    end
+                    
                     io_commands[2] = "MAXIMUM RECORD LENGTH  9999"
                     io_commands[3] = "SET DATA MISSING VALUE missing"
                     io_commands[4] = "SET READ MISSING VALUE 999"
@@ -208,7 +213,7 @@
                         #   Checking that not scientific notation is present
                             e_index = findall(x -> occursin("e", x), s_variable)
                             if (size(e_index)[1] > 0 )
-                                for j in 1:length(e_index)
+                                for j in eachindex(e_index)
                                     s_variable[e_index[j]] = Formatting.format(variable[e_index[j]])
                                 end
                             else 
